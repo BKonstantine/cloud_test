9779e329eada379b8457da54651c71f202cafd1a
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-06-06 19:00:22 +0300

    refactor: change main-page component and make link component

---
+++ src/components/UI/link/link.tsx
@@ -0,0 +1,26 @@
+import { FC } from "react";
+import style from "./link.module.css";
+import FolderIcon from "../../../icons/folder-icon";
+
+type Props = {
+  href: string;
+  text: string;
+};
+
+const Link: FC<Props> = ({ href, text }) => {
+  return (
+    <li className={style.container}>
+      <a
+        href={href}
+        target="_blank"
+        rel="noopener noreferrer"
+        className={style.link}
+      >
+        <FolderIcon />
+        {text}
+      </a>
+    </li>
+  );
+};
+
+export default Link;
--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
 import cn from "classnames";
-import FolderIcon from "../../icons/folder-icon";
+import Link from "../../components/UI/link/link";
 import ButtonLink from "../../components/UI/button-link/button-link";
 import Input from "../../components/UI/input/input";
 import style from "./main-page.module.css";
@@ -18,39 +18,12 @@ const MainPage: FC = () => {
         >
           <h1 className={style.profile__name}>Бузунов Костя</h1>
           <ul className={style.profile__links}>
-            <li className={style.profile__link}>
-              <a
-                href="https://t.me/kbuzunov"
-                target="_blank"
-                rel="noopener noreferrer"
-                className={style.link}
-              >
-                <FolderIcon />
-                Telegram
-              </a>
-            </li>
-            <li className={style.profile__link}>
-              <a
-                href="https://github.com/BKonstantine"
-                target="_blank"
-                rel="noopener noreferrer"
-                className={style.link}
-              >
-                <FolderIcon />
-                GitHub
-              </a>
-            </li>
-            <li className={style.profile__link}>
-              <a
-                href="https://file.notion.so/f/s/dc5ba534-6f6c-4bb9-8788-454c22e57c22/%D0%91%D1%83%D0%B7%D1%83%D0%BD%D0%BE%D0%B2_%D0%9A%D0%BE%D0%BD%D1%81%D1%82%D0%B0%D0%BD%D1%82%D0%B8%D0%BD_%D0%90%D0%BD%D0%B4%D1%80%D0%B5%D0%B5%D0%B2%D0%B8%D1%87.pdf?id=6d6afd34-3a07-40c1-9080-1ae16f7c2f94&table=block&spaceId=079a14fc-669a-4afd-bd77-e61c47945c97&expirationTimestamp=1686126253618&signature=hNIAhfMQbn2aWHVYIwmv_ubYzV1BWYkYcVfRLm2ZSk4&downloadName=%D0%91%D1%83%D0%B7%D1%83%D0%BD%D0%BE%D0%B2+%D0%9A%D0%BE%D0%BD%D1%81%D1%82%D0%B0%D0%BD%D1%82%D0%B8%D0%BD+%D0%90%D0%BD%D0%B4%D1%80%D0%B5%D0%B5%D0%B2%D0%B8%D1%87.pdf"
-                target="_blank"
-                rel="noopener noreferrer"
-                className={style.link}
-              >
-                <FolderIcon />
-                Resume
-              </a>
-            </li>
+            <Link href="https://t.me/kbuzunov" text="Telegram" />
+            <Link href="https://github.com/BKonstantine" text="GitHub" />
+            <Link
+              href="https://file.notion.so/f/s/dc5ba534-6f6c-4bb9-8788-454c22e57c22/%D0%91%D1%83%D0%B7%D1%83%D0%BD%D0%BE%D0%B2_%D0%9A%D0%BE%D0%BD%D1%81%D1%82%D0%B0%D0%BD%D1%82%D0%B8%D0%BD_%D0%90%D0%BD%D0%B4%D1%80%D0%B5%D0%B5%D0%B2%D0%B8%D1%87.pdf?id=6d6afd34-3a07-40c1-9080-1ae16f7c2f94&table=block&spaceId=079a14fc-669a-4afd-bd77-e61c47945c97&expirationTimestamp=1686126253618&signature=hNIAhfMQbn2aWHVYIwmv_ubYzV1BWYkYcVfRLm2ZSk4&downloadName=%D0%91%D1%83%D0%B7%D1%83%D0%BD%D0%BE%D0%B2+%D0%9A%D0%BE%D0%BD%D1%81%D1%82%D0%B0%D0%BD%D1%82%D0%B8%D0%BD+%D0%90%D0%BD%D0%B4%D1%80%D0%B5%D0%B5%D0%B2%D0%B8%D1%87.pdf"
+              text="Resume"
+            />
           </ul>
         </div>
       </header>
@@ -63,11 +36,11 @@ const MainPage: FC = () => {
         >
           <div className={style.input}>
             <label>Номер телефона</label>
-            <Input disabled value="+7 (924) 364-11-39"/>
+            <Input disabled value="+7 (924) 364-11-39" />
           </div>
           <div className={style.input}>
             <label>Email</label>
-            <Input disabled value="buzunov.constantine@yandex.ru"/>
+            <Input disabled value="buzunov.constantine@yandex.ru" />
           </div>
         </div>
         <ButtonLink to="/create">Начать</ButtonLink>
