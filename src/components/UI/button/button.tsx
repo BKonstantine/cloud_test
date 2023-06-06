9ab3a60aabfed3199dacbcb4a3e6dbc29248bcbd
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-06-06 19:49:59 +0300

    feat: add button component

---
+++ src/components/UI/button/button.tsx
@@ -0,0 +1,44 @@
+import { FC, HTMLProps, ReactNode, memo } from "react";
+import cn from "classnames";
+import style from "./button.module.css";
+
+type ButtonType = "button" | "submit" | "reset";
+type ButtonSize = "small" | "large";
+type ButtonColor = "primary" | "secondary";
+
+interface Props extends HTMLProps<HTMLButtonElement> {
+  type?: ButtonType;
+  buttonSize?: ButtonSize;
+  color?: ButtonColor;
+  children: ReactNode;
+}
+
+const Button: FC<Props> = memo(
+  ({
+    type = "button",
+    buttonSize = "small",
+    color = "primary",
+    children,
+    ...rest
+  }) => {
+    return (
+      <button
+        type={type}
+        className={cn(
+          style.button,
+          buttonSize === "small"
+            ? style.button_size_small
+            : style.button_size_large,
+          color === "primary"
+            ? style.button_color_primary
+            : style.button_color_secondary
+        )}
+        {...rest}
+      >
+        {children}
+      </button>
+    );
+  }
+);
+
+export default Button;
--- src/pages/form-page/form-page.tsx
+++ src/pages/form-page/form-page.tsx
@@ -1,8 +1,16 @@
 import { FC } from "react";
 import style from "./form-page.module.css";
+import Button from "../../components/UI/button/button";
 
 const FormPage: FC = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className={style.buttons}>
+        <Button color="secondary">Назад</Button>
+        <Button>Вперед</Button>
+      </div>
+    </div>
+  );
 };
 
 export default FormPage;
