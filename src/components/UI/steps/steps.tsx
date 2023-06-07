fd25fbf894347147fcefc6c8440dbfca4fdfb502
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-06-07 17:21:28 +0300

    feat: make steps component

--- src/components/UI/steps/steps.tsx
+++ src/components/UI/steps/steps.tsx
@@ -7,6 +7,30 @@ import style from "./steps.module.css";
 type Props = { step: number };
 
 const Steps: FC<Props> = memo(({ step }) => {
+  const checkTextColor = (position: number, step: number) => {
+    if (position <= step) {
+      return style.circle__number_active;
+    }
+  };
+
+  const checkIconColor = (position: number, step: number) => {
+    if (position <= step) {
+      return style.circle__item_active;
+    }
+  };
+
+  const checkIcon = (position: number, step: number) => {
+    /* if (position > step) {
+      return;
+    } */
+    if (position === step) {
+      return <PointIcon />;
+    }
+    if (position < step) {
+      return <ArrowIcon />;
+    }
+  };
+
   const checkStep = (step: number) => {
     return step === 2
       ? style.step_active_50
@@ -17,9 +41,47 @@ const Steps: FC<Props> = memo(({ step }) => {
 
   return (
     <div className={style.steps}>
-      <div className={style.circle}>
-        <div className={style.circle__item}></div>
-        <span className={style.circle__number}></span>
+      <div className={cn(style.circle, style.circle_position_left)}>
+        <div className={cn(style.circle__item, checkIconColor(1, step))}>
+          {checkIcon(1, step)}
+        </div>
+        <span
+          className={cn(
+            style.circle__number,
+            style.circle__number_position_left,
+            checkTextColor(1, step)
+          )}
+        >
+          1
+        </span>
+      </div>
+      <div className={cn(style.circle, style.circle_position_center)}>
+        <div className={cn(style.circle__item, checkIconColor(2, step))}>
+          {checkIcon(2, step)}
+        </div>
+        <span
+          className={cn(
+            style.circle__number,
+            style.circle__number_position_center,
+            checkTextColor(2, step)
+          )}
+        >
+          2
+        </span>
+      </div>
+      <div className={cn(style.circle, style.circle_position_right)}>
+        <div className={cn(style.circle__item, checkIconColor(3, step))}>
+          {checkIcon(3, step)}
+        </div>
+        <span
+          className={cn(
+            style.circle__number,
+            style.circle__number_position_right,
+            checkTextColor(3, step)
+          )}
+        >
+          3
+        </span>
       </div>
       <div className={cn(style.step, checkStep(step))}></div>
     </div>
