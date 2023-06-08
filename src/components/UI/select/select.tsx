84828d3092e4cfe51aeb6b396d20b858928bfe02
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-06-08 19:12:02 +0300

    feat: change select component

--- src/components/UI/select/select.tsx
+++ src/components/UI/select/select.tsx
@@ -1,32 +1,78 @@
-/* import { forwardRef, ForwardedRef } from "react";
-import ReactSelect, { Props as SelectProps } from "react-select";
-import style from "./select.module.css";
+import { FC } from "react";
+import ReactSelect from "react-select";
 
-type Options = {
+type Props = {
+  //TODO Исправить типизацию
+  options: any;
+  placeholder: string;
   value: string;
-  label: string;
+  onChange: () => void;
 };
 
-type Ref = ForwardedRef<Select>;
-
-type Props = SelectProps<Options> & {  
-  label?: string;
+const Select: FC<Props> = ({ options, placeholder, value, onChange }) => {
+  return (
+    <ReactSelect
+      value={value}
+      onChange={onChange}
+      options={options}
+      placeholder={placeholder}
+      styles={{
+        container: (style) => ({
+          ...style,
+          maxWidth: "300px",
+        }),
+        indicatorSeparator: (style) => ({
+          ...style,
+          display: "none",
+        }),
+        placeholder: (style) => ({
+          ...style,
+          fontWeight: "400",
+          fontSize: "14px",
+          lineHeight: "20px",
+          color: "rgba(0, 0, 0, 0.48)",
+          fontFamily: "inherit",
+        }),
+        control: (style, state) => ({
+          ...style,
+          borderColor: state.isFocused ? "black" : "rgba(0, 0, 0, 0.16)",
+          boxShadow: state.isFocused ? "0 0 0 1px black" : style.boxShadow,
+          "&:hover": {
+            borderColor: state.isFocused ? "black" : "rgba(0, 0, 0, 0.16)",
+          },
+          height: "44px",
+        }),
+        menu: (style) => ({
+          ...style,
+          margin: "0",
+        }),
+        menuList: (style) => ({
+          ...style,
+          boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.08)",
+          padding: "12px",
+        }),
+        option: (style, state) => ({
+          ...style,
+          fontSize: "14px",
+          lineHeight: "20px",
+          color: "#333333",
+          padding: "4px 0",
+          backgroundColor: "white",
+          "&:hover": {
+            backgroundColor: state.isFocused
+              ? "rgba(0, 0, 0, 0.16)"
+              : style.backgroundColor,
+          },
+        }),
+        singleValue: (style) => ({
+          ...style,
+          fontSize: "14px",
+          lineHeight: "20px",
+          color: "#333333",
+        }),
+      }}
+    />
+  );
 };
 
-const Select = forwardRef<Ref, Props>(
-  ({ options, placeholder, label, ...rest }, ref) => {
-    return (
-      <div className={style.select}>
-        {label && <label className={style.select__label}>{label}</label>}
-        <ReactSelect
-          options={options}
-          placeholder={placeholder}
-          {...rest}
-          ref={ref}
-        />
-      </div>
-    );
-  }
-);
-
-export default Select; */
+export default Select;
